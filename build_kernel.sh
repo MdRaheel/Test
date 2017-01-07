4cbe8172e337262a940ebd8caebbe9556a7512a5
MdRaheel <m.raheelxda@gmail.com>
2017-01-07 19:22:33 +0530

    Initial

---
+++ build_kernel.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export ARCH=arm
+export CROSS_COMPILE=/home/raheel/Desktop/Toolchain/hyper/bin/arm-eabi-
+mkdir output
+
+make -C $(pwd) O=output msm8916_sec_defconfig VARIANT_DEFCONFIG=msm8916_sec_j53g_eur_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+make -j64 -C $(pwd) O=output
+
+cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
